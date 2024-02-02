20be395a5b9bc9b6ba8f91433025670d4c9cffad
Alvaro Oliveira <alvaro.oliveira@compasso.com.br>
2024-02-02 16:49:29 -0300

    remove import

--- src/main/kotlin/br/com/cinemagazine/document/UserDocument.kt
+++ src/main/kotlin/br/com/cinemagazine/document/UserDocument.kt
@@ -1,7 +1,6 @@
 package br.com.cinemagazine.document
 
 import br.com.cinemagazine.constants.Gender
-import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
